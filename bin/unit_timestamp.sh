b91aeb90427e34a34522e5bdac84298f94548799
Peter Bui <pbui@nd.edu>
2024-08-23 10:37:40 -0400

    Update unit test scripts

--- bin/unit_timestamp.sh
+++ bin/unit_timestamp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-UNIT=bin/unit_timestamp
-WORKSPACE=/tmp/timestamp.$(id -u)
+UNIT=unit_timestamp
+WORKSPACE=/tmp/$UNIT.$(id -u)
 FAILURES=0
 
 error() {
@@ -16,26 +16,24 @@ cleanup() {
     exit $STATUS
 }
 
-export LD_LIBRARY_PATH=$LD_LIBRRARY_PATH:.
-
 mkdir $WORKSPACE
 
 trap "cleanup" EXIT
 trap "cleanup 1" INT TERM
 
-printf "Testing %s ...\n" "$(basename $UNIT)"
+echo "Testing $UNIT ..."
 
-if [ ! -x $UNIT ]; then
-    echo "Failure: $UNIT is not executable!"
+if [ ! -x bin/$UNIT ]; then
+    echo "Failure: bin/$UNIT is not executable!"
     exit 1
 fi
 
-TESTS=$($UNIT 2>&1 | tail -n 1 | awk '{print $1}')
+TESTS=$(bin/$UNIT 2>&1 | tail -n 1 | awk '{print $1}')
 for t in $(seq 0 $TESTS); do
-    desc=$($UNIT 2>&1 | awk "/$t/ { print \$3 }")
+    desc=$(bin/$UNIT 2>&1 | awk "/$t/ { print \$3 }")
 
     printf " %-60s ... " "$desc"
-    valgrind --leak-check=full $UNIT $t &> $WORKSPACE/test
+    valgrind --leak-check=full bin/$UNIT $t &> $WORKSPACE/test
     if [ $? -ne 0 ]; then
 	error "Failure (Exit Code)"
     elif [ $(awk '/ERROR SUMMARY:/ {print $4}' $WORKSPACE/test) -ne 0 ]; then
